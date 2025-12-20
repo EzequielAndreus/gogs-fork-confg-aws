a1afce940100c49e5bfbf379ee99754070e5a878
Poryoculus <rodrigopachecojor@gmail.com>
2025-12-19 23:16:43 -0300

    PFM-110 Ansible Design
    This is the Ansible design for aws with a dynamic inventory

---
+++ ansible/dynamicInventory.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Go to the Terraform folder
+cd infra || exit 1
+
+terraform init -reconfigure -backend=true
+
+# Get EC2 public IP from Terraform
+EC2_IP=$(terraform output -raw ec2_public_ip)
+
+# Validate IP
+if [[ -z "$EC2_IP" ]]; then
+  echo "Could not obtain the public IP address of the EC2"
+  exit 1
+fi
+
+# Return to project root
+cd ..
+
+# Generate the Ansible inventory
+cat <<EOF >ansible/inventory.ini
+[gogs_server]
+gogs ansible_host=$EC2_IP ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/gogs_key.pem
+EOF
+
+echo "inventory.ini file generated with IP: $EC2_IP"
